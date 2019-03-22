a1b4e9dfbf6feb552c7dae4e2d61e57740af841c
Matt <wetbikeboy2500@gmail.com>
2019-03-22 18:10:19 -0400

    major improvements and design ideas
    A class that controls the output is probably going to be the most important thing. There needs to be a layer of abstraction where console input and output are limited.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Entoli
 An interactive Cli that pays attention to detail
+
+### Why?
+I look at all these different Cli interfaces but none of them do what I want. They have all adopted a very similar format with a lot of issues attached. I want a Cli that I can use with confidence.
--- index.js
+++ index.js
@@ -5,10 +5,145 @@ console.log(chalk.blue('Hello world!'));
 const readline = require('readline');
 readline.emitKeypressEvents(process.stdin);
 process.stdin.setRawMode(true);
+
+
+class Ouputter {
+    constructor(arr) {
+        //doesn't do much now
+        for (let a of arr) {
+            this[a] = null;
+        }
+
+        this.__logging = false;
+        this.__hasOutput = false;
+
+        this.setOutputString();
+
+        this.__outputting = false;
+    }
+
+    startLogging () {
+        this.__logging = true;
+    }
+
+    stopLogging () {
+        this.__logging = false;
+    }
+
+    //input updates
+    update (inputStuff) {
+        for (let a of inputStuff) {
+            this[a.name] = a.value;
+        }
+
+    }
+
+    setOutputString (outputArray = []) {
+        outputArray = [
+            `Select an option`,
+            `\n`,
+            () => this[`i`], //this may have to change or work differently
+            `) Hello world`,
+            `\n`,
+            () => this[`i`] + 1,
+            `) Exit`,
+            `\n`,
+            `Last key pressed, `,
+            () => this[`key`],
+            `, `,
+            () => this[`o`]
+        ];
+
+        this.__string = outputArray;
+    }
+
+    output () {
+        if (!this.__outputting) {
+            this.__outputting = true;
+
+            let o = this.__string.map((a) => {
+                if (typeof a == 'function') {
+                    return a();
+                }
+                return a;
+            }).join(``);
+
+
+            this.coolClear(2);
+
+            process.stdout.cursorTo(0);
+            process.stdout.write(o);
+            //process.stdout.cursorTo(0);//this makes the cursor look so much better than having weird midplaced selector
+
+            this.__hasOutput = true;
+
+            setTimeout(() => {
+                this.__outputting = false;
+            }, 100);
+        }
+    }
+
+    clear () {
+        if (this.__hasOutput) {
+            process.stdout.moveCursor(0, -3);
+        }
+
+        process.stdout.clearLine();
+    }
+
+    coolClear (limit = 2) {
+        for (let i = 0; i <= limit; i++) {
+            process.stdout.clearLine();
+            process.stdout.moveCursor(0, -1);
+        }
+        process.stdout.clearLine();
+
+        process.stdout.cursorTo(0);
+    }
+}
+
+let outp = new Ouputter([`i`, `key`, `o`]);
+
+outp.output();
+
+let e =``;
+
 process.stdin.on('keypress', (str, key) => {
     if (key.ctrl && key.name === 'c') {
         process.exit();
     } else {
+
+        if (key.name == 'e') {
+            outp.coolClear();
+
+            return;
+        }
+
+        if (str)
+        e += str;
+
+        i += 1;
+
+        outp.update([
+            {
+                name: 'i',
+                value: i
+            },
+            {
+                name: 'key',
+                value: key.name
+            },
+            {
+                name: 'o',
+                value: e
+            }
+        ]);
+
+        outp.output();
+
+        /*
+        console.log(key);
+
         if (key.name == "up" || key.name == "down") {
             if (i == 0) {
                 i = 1;
@@ -16,18 +151,15 @@ process.stdin.on('keypress', (str, key) => {
                 i = 0;
             }
             printProgress(key.name);
-        }
-
-        if (key.name == "enter") {
+        } else if (key.name == "enter") {
             process.exit(0);
-        }
+        } else {
+
+        }*/
     }
 });
-console.log('Press any key...');
 
 let resetting = false;
-
-let t = 0;
 
 let i = 0;
 
@@ -52,6 +184,8 @@ function printProgress (key) {
         }, 100);
     }
 }
+
+
 
 /*
 const rl = readline.createInterface({
