aebd7392969065e8e57b062ca1967a93d6da84ab
Matt <wetbikeboy2500@gmail.com>
2019-09-26 13:15:50 -0400

    added to index for simple examples

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 import EntoliPrompt from './SimpleEntoliPrompt';
+import EntoliList from './EntoliList';
+import EntoliListMulti from './EntoliListMulti';
 
 let El = new EntoliPrompt('h');
 
-El().then((a) => {});
+El().then((a) => {
+    console.log(a);
+    
+    let EL = new EntoliList([
+        ['Name 1', 'Value 1'],
+        ['Name 2', 'Value 2']
+    ]);
+    
+    EL().then((a) => {
+        console.log(a);
+        
+        let ELM = new EntoliListMulti([
+            ['Name 1', 'Value 1'],
+            ['Name 2', 'Value 2'],
+            ['Name 3', 'Value 3'],
+            ['Name 4', 'Value 4'],
+            ['Name 5', 'Value 5'],
+        ]);
+        
+        ELM().then((a) => {
+            console.log(a);
+        });
+    });
+    
+});
+
+
 
