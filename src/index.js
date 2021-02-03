0fbae9d9b3982e75e3fdaa6e6ccbd01ca9911d6f
Matt <wetbikeboy2500@gmail.com>
2021-02-03 18:57:16 -0500

    Update chalk

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ let El = new EntoliPrompt('h');
 
 El().then((a) => {
     console.log(a);
-    
+
     let EL = new EntoliList([
         ['Name 1', 'Value 1'],
         ['Name 2', 'Value 2']
     ]);
-    
+
     EL().then((a) => {
         console.log(a);
-        
+
         let ELM = new EntoliListMulti([
             ['Name 1', 'Value 1'],
             ['Name 2', 'Value 2'],
@@ -22,12 +22,12 @@ El().then((a) => {
             ['Name 4', 'Value 4'],
             ['Name 5', 'Value 5'],
         ]);
-        
+
         ELM().then((a) => {
             console.log(a);
         });
     });
-    
+
 });
 
 
